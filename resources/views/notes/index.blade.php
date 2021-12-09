6873ea74f164feb121311b5850240657d5050dbc
Andrew Flannery <andyflan@inheritweb.net>
2021-12-09 14:20:03 +0000

    Add some ui/nav stuff

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -5,8 +5,12 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="m-4 items-end">
+                <a class="btn btn-green" href="{{ route('notes.create') }}">Add note</a>
+            </div>
+            
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @foreach ($notes as $note)
